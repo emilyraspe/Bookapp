dbbe84b36103d177797e7dfeb8855c0b7ed3d12c
Emily Raspe <emilyraspe@icloud.com>
2024-04-12 15:37:31 +0200

    edit fetch in [id]

--- pages/books/[id].js
+++ pages/books/[id].js
@@ -2,30 +2,28 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { useEffect } from "react";
 import BookDetails from "../../components/BookDetails/BookDetails";
-import Books from "../../components/Books/Books";
+import useSWR from "swr";
+
+const fetcher = async (url) => await fetch(url).then((res) => res.json());
 
 export default function BookDetailsPage({ books }) {
   const router = useRouter();
   const { id } = router.query;
-  const [selectedBook, setSelectedBook] = useState();
-  console.log("=====", id);
-  useEffect(() => {
-    setSelectedBook(books?.find((book) => book.id === id));
-  }, [books, id]);
-  if (!id) {
-    return null;
-  }
-  console.log("BookDetailsPage, selectedBook", selectedBook);
 
-  if (selectedBook) {
+  const bookURL = `https://www.googleapis.com/books/v1/volumes/${id}`;
+  const { data, error } = useSWR(bookURL, fetcher);
+  console.log(bookURL);
+  console.log("==========", data);
+
+  if (data) {
     return (
       <BookDetails
-        name={selectedBook.volumeInfo.title}
-        authors={selectedBook.volumeInfo.authors}
-        genre={selectedBook.volumeInfo.categories}
-        image={selectedBook.volumeInfo.imageLinks.thumbnail}
-        published={selectedBook.volumeInfo.publishedDate}
-        description={selectedBook.volumeInfo.description}
+        name={data.volumeInfo.title}
+        authors={data.volumeInfo.authors}
+        genre={data.volumeInfo.categories}
+        image={data.volumeInfo.imageLinks.thumbnail}
+        published={data.volumeInfo.publishedDate}
+        description={data.volumeInfo.description}
       />
     );
   }
