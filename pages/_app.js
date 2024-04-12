7eba1ba972666c0731f5643d03c1a1a072f25820
Emily Raspe <emilyraspe@icloud.com>
2024-04-12 10:22:13 +0200

    fix searchbar and adjust searchQuery

--- pages/_app.js
+++ pages/_app.js
@@ -12,12 +12,17 @@ export default function App({ Component, pageProps }) {
   }
 
   const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-  const googleBooksURL = `https://www.googleapis.com/books/v1/volumes?q=${input}&maxResults=5&key=${API_KEY}`;
+  const searchQuery = input ? `q=${input}` : "";
+  const googleBooksURL = `https://www.googleapis.com/books/v1/volumes?${searchQuery}&maxResults=5&key=${API_KEY}`;
 
-  const { data } = useSWR(googleBooksURL, fetcher);
+  const { data, error } = useSWR(googleBooksURL, fetcher);
+
+  if (error) {
+    return <p>Error loading books. Please try again later.</p>;
+  }
 
   if (!data) {
-    return;
+    return <p>Search for a book</p>;
   }
   const books = data.items;
   console.log("books", data.items);
