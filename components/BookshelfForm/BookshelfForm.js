c057404577f78d771c8ca1ef9d04c4adc85fd3c6
Emily Raspe <emilyraspe@icloud.com>
2024-04-15 19:19:58 +0200

    add functionality to form

--- components/BookshelfForm/BookshelfForm.js
+++ components/BookshelfForm/BookshelfForm.js
@@ -1,9 +1,40 @@
+import { useState } from "react";
+
 export default function BookshelfForm() {
+  const [bookshelves, setBookshelves] = useState([]);
+  const now = new Date();
+  const dateTime = now.toLocaleString();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const { value } = event.target.elements.bookshelf; // Get the value from the input field
+    const newBookshelf = {
+      name: value,
+      created: dateTime,
+    };
+    setBookshelves([...bookshelves, newBookshelf]);
+    event.target.reset();
+  }
+
   return (
-    <form>
-      <label>Name of bookshelf</label>
-      <input type="text"></input>
-      <button type="submit">Create bookshelf</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>Name of bookshelf</label>
+        <input type="text" name="bookshelf"></input>{" "}
+        <button type="submit">Create bookshelf</button>
+      </form>
+      <div>
+        <h2>Bookshelves</h2>
+        <ul>
+          {bookshelves.map((shelf, index) => (
+            <a href="#">
+              <li key={index}>
+                {shelf.name} - Created: {shelf.created}
+              </li>
+            </a>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -6,9 +6,6 @@ export default function Profile({ session }) {
       <h1>{session.user.name}</h1>
       <img src={session.user.image} />
       <BookshelfForm />
-
-      <h2>My bookshelfs</h2>
-      <p>nothing here yet...</p>
     </>
   );
 }
