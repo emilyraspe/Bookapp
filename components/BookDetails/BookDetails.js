3b6761ee4ca88e47ae58e43a85f6057f3996ee74
Emily Raspe <emilyraspe@icloud.com>
2024-04-18 23:09:32 +0200

    fixes to button and details page if user is not logged in

--- components/BookDetails/BookDetails.js
+++ components/BookDetails/BookDetails.js
@@ -35,62 +35,77 @@ export default function BookDetails({
       return;
     }
 
-    //find bookarray of current user
-    const readBooksOfUser = readBooks.find(
-      (obj) => obj.userId === session.user.userId
-    );
-
-    if (readBooksOfUser) {
-      //find current book in array
-      const currentBook = readBooksOfUser.books.find(
-        (book) => book.id === bookdata.items[0].id
+    if (session) {
+      //find bookarray of current user
+      const readBooksOfUser = readBooks.find(
+        (obj) => obj.userId === session.user.userId
       );
-      if (currentBook) {
-        console.log("Current book found:", currentBook);
-        return;
-      } else {
-        console.log("Current book not found");
 
-        await fetch(`/api/readBooks/`, {
-          method: "PUT",
-          body: JSON.stringify({
-            book: bookWithDate,
-            userId: session.user.userId,
-            date: dateTime,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        mutate();
+      if (readBooksOfUser) {
+        //find current book in array
+        const currentBook = readBooksOfUser.books.find(
+          (book) => book.id === bookdata.items[0].id
+        );
+        if (currentBook) {
+          console.log("Current book found:", currentBook);
+          return;
+        } else {
+          console.log("Current book not found");
+
+          await fetch(`/api/readBooks/`, {
+            method: "PUT",
+            body: JSON.stringify({
+              book: bookWithDate,
+              userId: session.user.userId,
+              date: dateTime,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          mutate();
+        }
       }
     }
   }
 
   //diable button if books is already in array - returns true or false
-  const isBookFound =
-    readBooks &&
-    readBooks.some(
+
+  const isBookFound = () => {
+    if (!session || !readBooks) {
+      return false; // Return false if session or readBooks is not available
+    }
+
+    return readBooks.some(
       (obj) =>
         obj.userId === session.user.userId &&
         obj.books.some((book) => book.id === bookdata.items[0].id)
     );
+  };
+
   return (
     <div>
       <h1>{name}</h1>
-      <img src={image} height={350}></img>
+      <img src={image} height={350} alt={name} />
       <h4>Author: {authors}</h4>
       <p>Published: {published}</p>
       <p>Publisher: {publisher}</p>
       <p>{description}</p>
-      {categories.map((categorie) => (
-        <div>{categorie}</div>
+      {categories.map((category, index) => (
+        <div key={index}>{category}</div>
       ))}
 
-      <button onClick={addToReadBooks} disabled={isBookFound}>
-        {isBookFound ? "Already Read" : "Mark as read"}
-      </button>
-      <p> {isBookFound ? `Was marked as read on ${currentBook.date}` : ""}</p>
+      {session ? (
+        <>
+          <button onClick={addToReadBooks} disabled={isBookFound()}>
+            {isBookFound() ? "Already Read" : "Mark as read"}
+          </button>
+          <p>{isBookFound() ? "Was marked as read on" : ""}</p>
+        </>
+      ) : (
+        ""
+      )}
+
       <AddToBookshelfForm bookdata={bookdata} />
     </div>
   );
