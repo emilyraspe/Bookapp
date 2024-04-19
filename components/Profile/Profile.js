5ad5db40237eceb5025d121ebd00e89b7caad642
Emily Raspe <emilyraspe@icloud.com>
2024-04-19 16:15:16 +0200

    basic structure of achievments

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -1,12 +1,31 @@
 import Link from "next/link";
 import BookshelfForm from "../BookshelfForm/BookshelfForm";
+import Archievment from "../Achievement/Achievement";
+import useSWR from "swr";
 
 export default function Profile({ session }) {
+  //ReadBooks Data
+  const { data: readBooksData, error: readBooksError } =
+    useSWR("/api/readBooks/");
+
+  const userBooks = readBooksData?.filter(
+    (item) => item.userId === session.user.userId
+  );
+
+  let booksReadNumber = 0;
+  if (userBooks) {
+    booksReadNumber = userBooks[0].books.length;
+  }
+  console.log("BooksReadNumber", booksReadNumber);
+
   return (
     <>
-      <h1>{session.user.name}</h1>
       <img src={session.user.image} />
-      <Link href="/markedasread">Books that I read</Link>
+      <h1>{session.user.name}</h1>
+      <Link href="/markedasread">
+        You read {booksReadNumber} Books. See all
+      </Link>
+      <Archievment booksReadNumber={booksReadNumber} />
       <BookshelfForm />
     </>
   );
