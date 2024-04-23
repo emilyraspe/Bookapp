785459dc3e571e0b1037459bd4062e54ebfb6c45
Emily Raspe <emilyraspe@icloud.com>
2024-04-23 10:14:06 +0200

    add archievment for 20 books

--- components/Archievement/Archievement.js
+++ components/Archievement/Archievement.js
@@ -1,18 +1,16 @@
-import Archievment10 from "./Archivment-10";
-import Archievment50 from "./Archivment-50";
+import Archievment10 from "./Archievment-10";
+import Archievment20 from "./Archievment-20";
+import Archievment50 from "./Archievment-50";
 
 export default function Archievment({ booksReadNumber }) {
-  if (booksReadNumber > 10) {
-  }
-  if (booksReadNumber > 50) {
-  }
   return (
     <>
-      <p className="tagline small-text">
+      <p className="tagline small-text link-underline">
         Archievments for reading {booksReadNumber} books
       </p>
       <div className="archievments-container">
-        {booksReadNumber >= 20 ? <Archievment10 /> : "No archievments yet"}
+        {booksReadNumber >= 10 ? <Archievment10 /> : "No archievments yet"}
+        {booksReadNumber >= 20 ? <Archievment20 /> : ""}
         {booksReadNumber >= 50 ? <Archievment50 /> : ""}
       </div>
     </>
