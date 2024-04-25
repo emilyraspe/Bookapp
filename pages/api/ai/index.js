487801975645e34af72f00fa137cd32e695fb352
Emily Raspe <emilyraspe@icloud.com>
2024-04-25 11:49:35 +0200

    copied bookrecommendation code in here

---
+++ pages/api/ai/index.js
@@ -0,0 +1,25 @@
+import Anthropic from "@anthropic-ai/sdk";
+
+const anthropic = new Anthropic({
+  apiKey: process.env["ANTHROPIC_API_KEY"],
+});
+
+async function handler(req, res) {
+  const { searchInput } = req.body;
+
+  const message = await anthropic.messages.create({
+    max_tokens: 1024,
+    messages: [
+      {
+        role: "user",
+        content: `Give me one book recommendation based on ${searchInput}. Only answer with the title and author of the book. Don't recommend books from the same author.`,
+      },
+    ],
+    model: "claude-3-opus-20240229",
+  });
+
+  console.log("Hallo", message);
+  res.status(200).json(message.content);
+}
+
+export default handler;
--- pages/recommendations/index.js
+++ pages/recommendations/index.js
@@ -1,5 +1,47 @@
-import Recommendations from "../../components/Recommendations/Recommendations";
+import { useState } from "react";
 
-export default function ProfilePage() {
-  return <Recommendations />;
+export default function Rec() {
+  const [data, SetDate] = useState("");
+  async function handleClick(event) {
+    event.preventDefault();
+    const inputValue = event.target.elements.fav.value;
+
+    const searchInput = inputValue.trim();
+
+    if (!searchInput) {
+      console.error("Input value is empty");
+      return;
+    }
+
+    try {
+      const response = await fetch(`/api/ai/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({ searchInput }),
+      });
+      const responseData = await response.json();
+      console.log("Response Data:", responseData);
+      SetDate(responseData);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    console.log("SearchInput", searchInput);
+    event.target.reset();
+  }
+  console.log("===", data);
+
+  return (
+    <div>
+      <h1>Get a book recommendation</h1>
+      <form onSubmit={handleClick}>
+        <input type="text" name="fav"></input>
+        <label>Whats your favourite Book or Author?</label>
+        <button type="submit">Submit</button>
+      </form>
+      {data && <p>{data[0].text}</p>}
+    </div>
+  );
 }
