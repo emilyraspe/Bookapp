086e6f1f79f24862815004a44f28f2849caf828d
Emily Raspe <emilyraspe@icloud.com>
2024-04-26 11:28:10 +0200

    active state to navbar

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,20 +1,42 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Navigation() {
-  const path = "search";
+  const router = useRouter();
+  const [currentPath, setCurrentPath] = useState("");
+
+  useEffect(() => {
+    setCurrentPath(router.pathname);
+  }, [router.pathname]);
 
   return (
     <>
       <nav className="nav">
-        <Link href="/" className="nav--element nav--button nav--element-left">
+        <Link
+          href="/"
+          className={`nav--element nav--button nav--element-left ${
+            currentPath === "/" ? "active" : ""
+          }`}
+        >
           Start
         </Link>
 
-        <Link href="/search" className="nav--element nav--button">
+        <Link
+          href="/search"
+          className={`nav--element nav--button ${
+            currentPath === "/search" ? "active" : ""
+          }`}
+        >
           Search
         </Link>
 
-        <Link href="/profile" className="nav--element nav--button">
+        <Link
+          href="/profile"
+          className={`nav--element nav--button  ${
+            currentPath === "/profile" ? "active" : ""
+          }`}
+        >
           Profile
         </Link>
       </nav>
