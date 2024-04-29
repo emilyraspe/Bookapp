03e1cc27d40da892e68f3f51ecf358f723aca09a
Emily Raspe <emilyraspe@icloud.com>
2024-04-29 12:00:51 +0200

    console

--- pages/markedAsRead/index.js
+++ pages/markedAsRead/index.js
@@ -1,6 +1,7 @@
 import MarkedAsRead from "../../components/MarkedAsRead/MarkedAsRead";
 
 export default function MarkedAsReadPage() {
+  console.log("hi");
   return (
     <>
       <MarkedAsRead />
